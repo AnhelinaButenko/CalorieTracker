afeffd00fd12f4d8ab26510708f4b014465c1cbc
Angelina Butenko <angelinaa2712@gmail.com>
2023-11-09 17:59:52 +0200

    add product controller

---
+++ CalorieTracker.Api/Controllers/AppController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalorieTracker.Api.Controllers;
+
+public class AppController : Controller
+{
+
+}
---
+++ CalorieTracker.Api/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using CalorieTracker.Data.Repository;
+using CalorieTracker.Domains;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace CalorieTracker.Api.Controllers;
+
+[Route("api/[Controller]")]
+[ApiController]
+[Produces("application/json")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductRepository _repository;
+    private readonly IMapper _mapper;
+
+    public ProductController(IProductRepository repository,
+        IMapper mapper)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    [HttpGet]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(400)]
+    public async Task<ActionResult<IEnumerable<Product>>> Get()
+    {
+        var product = await _repository.GetAll();
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(_mapper.Map<Product>(product));
+    }
+}
