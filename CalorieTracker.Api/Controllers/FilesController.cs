fb88be1b682c93a6139ed2877bc58fd82fe08ee9
Angelina Butenko <angelinaa2712@gmail.com>
2023-11-24 19:16:31 +0200

    add files controller

---
+++ CalorieTracker.Api/Controllers/FilesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CalorieTracker.Api.Controllers;
+
+[Route("api/files")]
+[ApiController]
+public class FilesController : ControllerBase
+{
+    private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+    public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+    {
+        _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+            ?? throw new System.ArgumentException(
+                nameof(fileExtensionContentTypeProvider));
+    }
+
+    [HttpGet("{fileId}")]
+    public ActionResult GetFile(string fileId)
+    {
+        string pathToFile = "ProductsDb.json";
+
+        if (!System.IO.File.Exists(pathToFile))
+        {
+            return NotFound();
+        }
+
+        if (!_fileExtensionContentTypeProvider.TryGetContentType(
+            pathToFile, out var contentType))
+        {
+            contentType = "application/octet-stream";
+        }
+
+        var bytes = System.IO.File.ReadAllBytes(pathToFile);
+
+        return File(bytes, contentType, Path.GetFileName(pathToFile));
+    }
+}
